b83b7be408811354afb130d727bb817c595e8413
Karan Kumar <114342084+Karannkx@users.noreply.github.com>
2024-01-20 19:13:29 +0530

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBtffgn7J7C2uU2alpDFfaU0l_vgpS5VQM",
+    authDomain: "testx-9edfa.firebaseapp.com",
+    projectId: "testx-9edfa",
+    storageBucket: "testx-9edfa.appspot.com",
+    messagingSenderId: "1049256354402",
+    appId: "1:1049256354402:web:e8fe1ad0ad12d4e472a73b",
+    measurementId: "G-6DFPBLP2VP"
+};
+firebase.initializeApp(firebaseConfig);
